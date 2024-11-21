65924d8b7898f4a05e3cb7b07b1d1606275329ca
sudarsh <sudarshwazza382@gmail.com>
2024-11-21 09:31:06 +0530

    mongourifix

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,7 +2,13 @@ import mongoose from 'mongoose'
 
 const connectDb = async (): Promise<void> => {
     try {
-        await mongoose.connect(process.env.MONGO_URI as string)
+        const mongoUri = process.env.MONGO_URI as string;
+
+        if (!mongoUri) {
+            throw new Error('MONGO_URI is not defined in the environment variables');
+        }
+
+        await mongoose.connect(mongoUri);
     } catch (error) {
         console.error('Error connecting to MongoDB Atlas',error)
     }
